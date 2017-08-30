fe3f79ddd9f4db90340251f23ea344cbb1dd2f74
Umherirrender <umherirrender_de.wp@web.de>
2017-08-30 22:23:58 +0200

    Rename entry point to match extension name
    The jenkins unit test job needs a entry point
    which is same named as the git repository
    
    Change-Id: Iad0e6942d167ddf53d3d7df0133d96cfe114967b

--- Link_Attributes.php
+++ Link_Attributes.php
@@ -13,7 +13,7 @@
 if (!defined('MEDIAWIKI')) {
    echo <<<EOT
 To install my extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/LinkAttributes/LinkAttributes.php" );
+require_once( "\$IP/extensions/LinkAttributes/Link_Attributes.php" );
 EOT;
    exit( 1 );
 }
