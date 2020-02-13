3744eb9a399af5756fd598f58a7db830ec9be355
Sam Wilson <sam@samwilson.id.au>
2020-02-13 10:07:00 +0800

    Switch to extension registration and improve
    Add extension.json, use more modern hook, fix some coding style
    issues, and delete INSTALL file in favour of the on-wiki
    documentation.
    
    Bug: T98668
    Change-Id: Ic92aa2a2215229813f21c1357e9e8b39ac45ae43

--- Link_Attributes.php
+++ Link_Attributes.php
@@ -3,88 +3,18 @@
  * Link Attributes - easy modification of rel/rev/class on <a> elements.
  *
  * @author Toby Inkster <http://tobyinkster.co.uk/>
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @license gpl-2.0-or-later
  */
- 
-/**
- * Protect against register_globals vulnerabilities.
- * This line must be present before any global variable is referenced.
- */
-if (!defined('MEDIAWIKI')) {
-   echo <<<EOT
-To install my extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/LinkAttributes/Link_Attributes.php" );
-EOT;
-   exit( 1 );
-}
- 
-// Extension credits that will show up on Special:Version    
-$wgExtensionCredits['parserhook'][] = array(
-  'path'           => __FILE__,
-	'name'           => 'Link Attributes',
-	'version'        => '1.1',
-	'author'         => array('Toby Inkster', 'Dennis Roczek'), 
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:Link_Attributes',
-	'descriptionmsg' => 'linkattributes-desc',
-  'license-name'   => "GPL-2.0-or-later"
-);
- 
-$wgHooks['LinkEnd'][] = 'linkattr_InternalLink';
-$wgHooks['LinkerMakeExternalLink'][] = 'linkattr_ExternalLink';
 
-$wgMessagesDirs['LinkAttributes'] = __DIR__ . '/i18n';
- 
-function linkattr_ModifyLink (&$text, &$attribs, $isExternal = 0)
-{
-	if ( preg_match('/^(.+)\(\((.*)\)\)$/', $text, $matches) )
-	{
-		$text = trim($matches[1]);
-		$rels = preg_split('/\s+/', $matches[2]);
- 
-		foreach ($rels as $r)
-		{
-			if ($isExternal && (strtolower($r)=='-nofollow'))
-				continue; # Not allowed!!
-		
-			if ((substr($r, 0, 2) == '-~' || substr($r, 0, 2) == '~-') && isset($attribs['rev']))
-				$attribs['rev'] = str_ireplace(substr($r, 2), '', $attribs['rev']);
-			elseif ((substr($r, 0, 2) == '-.' || substr($r, 0, 2) == '.-') && isset($attribs['class']))
-				$attribs['class'] = str_ireplace(substr($r, 2), '', $attribs['class']);
-			elseif ((substr($r, 0, 1) == '-') && isset($attribs['rel']))
-				$attribs['rel'] = str_ireplace(substr($r, 1), '', $attribs['rel']);
-			elseif (substr($r, 0, 1) == '~')
-				$attribs['rev'] .= ' ' . substr($r, 1);
-			elseif (substr($r, 0, 1) == '.')
-				$attribs['class'] .= ' ' . substr($r, 1);
-			else
-				$attribs['rel'] .= ' ' . $r;
-		}
- 
-		if (isset($attribs['rel']))
-			$attribs['rel'] = trim(preg_replace('/\s+/', ' ', $attribs['rel']));
-		if (isset($attribs['rev']))
-			$attribs['rev'] = trim(preg_replace('/\s+/', ' ', $attribs['rev']));
-		if (isset($attribs['class']))
-			$attribs['class'] = trim(preg_replace('/\s+/', ' ', $attribs['class']));
-	}	
-}
- 
-function linkattr_InternalLink ($skin, $target, $options, &$text, &$attribs, &$ret)
-{
-	linkattr_ModifyLink($text, $attribs);
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Link_Attributes' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Link_Attributes'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for Link_Attributes extension. Please use wfLoadExtension ' .
+		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 	return true;
-}
- 
-function linkattr_ExternalLink (&$url, &$text, &$link, &$attribs, $linktype)
-{
-	$attribsText = Html::expandAttributes( array( 'class' => 'external '.$linktype ) );
-	$mergedattribs = array_merge($attribs, Sanitizer::decodeTagAttributes($attribsText));
- 
-	linkattr_ModifyLink($text, $mergedattribs, 1);
-	if ($mergedattribs)
-			$attribsText = Xml::expandAttributes( $mergedattribs );
- 
-	$link = sprintf('<a href="%s"%s>%s</a>', $url, $attribsText, $text);
- 
-	return false;	
+} else {
+	die( 'This version of the Link_Attributes extension requires MediaWiki 1.29+' );
 }
---
+++ includes/Hooks.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace MediaWiki\Extension\LinkAttributes;
+
+use Html;
+use HtmlArmor;
+use MediaWiki\Linker\LinkRenderer;
+use MediaWiki\Linker\LinkTarget;
+use Sanitizer;
+use Xml;
+
+class Hooks {
+
+	/**
+	 * @param string|HtmlArmor &$text
+	 * @param array &$attribs
+	 * @param int $isExternal
+	 */
+	protected static function modifyLink( &$text, &$attribs, $isExternal = 0 ) {
+		if ( $text instanceof HtmlArmor ) {
+			$text = HtmlArmor::getHtml( $text );
+		}
+		if ( preg_match( '/^(.+)\(\((.*)\)\)$/', $text, $matches ) ) {
+			$text = trim( $matches[1] );
+			$rels = preg_split( '/\s+/', $matches[ 2 ] );
+
+			foreach ( $rels as $r ) {
+				if ( $isExternal && ( strtolower( $r ) == '-nofollow' ) ) {
+					continue; # Not allowed!!
+				}
+
+				if ( ( substr( $r, 0, 2 ) == '-~' || substr( $r, 0, 2 ) == '~-' ) && isset( $attribs[ 'rev' ] ) ) {
+					$attribs[ 'rev' ] = str_ireplace( substr( $r, 2 ), '', $attribs[ 'rev' ] );
+				} elseif ( ( substr( $r, 0, 2 ) == '-.' || substr( $r, 0, 2 ) == '.-' ) && isset( $attribs[ 'class' ] ) ) {
+					$attribs[ 'class' ] = str_ireplace( substr( $r, 2 ), '', $attribs[ 'class' ] );
+				} elseif ( ( substr( $r, 0, 1 ) == '-' ) && isset( $attribs[ 'rel' ] ) ) {
+					$attribs[ 'rel' ] = str_ireplace( substr( $r, 1 ), '', $attribs[ 'rel' ] );
+				} elseif ( substr( $r, 0, 1 ) == '~' ) {
+					$attribs[ 'rev' ] .= ' ' . substr( $r, 1 );
+				} elseif ( substr( $r, 0, 1 ) == '.' ) {
+					$attribs[ 'class' ] .= ' ' . substr( $r, 1 );
+				} else {
+					$attribs[ 'rel' ] .= ' ' . $r;
+				}
+			}
+
+			if ( isset( $attribs[ 'rel' ] ) )
+				$attribs[ 'rel' ] = trim( preg_replace( '/\s+/', ' ', $attribs[ 'rel' ] ) );
+			if ( isset( $attribs[ 'rev' ] ) )
+				$attribs[ 'rev' ] = trim( preg_replace( '/\s+/', ' ', $attribs[ 'rev' ] ) );
+			if ( isset( $attribs[ 'class' ] ) )
+				$attribs[ 'class' ] = trim( preg_replace( '/\s+/', ' ', $attribs[ 'class' ] ) );
+		}
+	}
+
+	/**
+	 * @link https://www.mediawiki.org/wiki/Manual:Hooks/HtmlPageLinkRendererEnd
+	 * @param LinkRenderer $linkRenderer
+	 * @param LinkTarget $target
+	 * @param bool $isKnown
+	 * @param string &$text
+	 * @param string[] &$attribs
+	 * @param string &$ret
+	 * @return bool
+	 */
+	public static function onHtmlPageLinkRendererEnd(
+		LinkRenderer $linkRenderer, LinkTarget $target, $isKnown, &$text, &$attribs, &$ret
+	) {
+		static::modifyLink( $text, $attribs );
+		return true;
+	}
+
+	/**
+	 * @link https://www.mediawiki.org/wiki/Manual:Hooks/LinkerMakeExternalLink
+	 * @param string &$url
+	 * @param string &$text
+	 * @param string &$link
+	 * @param string[] &$attribs
+	 * @param string $linktype
+	 * @return bool
+	 */
+	public static function onLinkerMakeExternalLink( &$url, &$text, &$link, &$attribs, $linktype ) {
+		$attribsText = Html::expandAttributes( [ 'class' => 'external ' . $linktype ] );
+		$mergedattribs = array_merge( $attribs, Sanitizer::decodeTagAttributes( $attribsText ) );
+
+		static::modifyLink( $text, $mergedattribs, 1 );
+		if ( $mergedattribs ) {
+			$attribsText = Xml::expandAttributes( $mergedattribs );
+		}
+		$link = sprintf( '<a href="%s"%s>%s</a>', $url, $attribsText, $text );
+
+		return false;
+	}
+}
